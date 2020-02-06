35451b2b613e5507ea912836634c46936f2af91f
Simon Riepl <s.riepl1@googlemail.com>
2020-02-06 23:56:50 +0100

    digit width methods

--- helper.py
+++ helper.py
@@ -5,16 +5,30 @@ def make_string_from_list(liste):
     return res[0:-1]
 
 def get_digits(number):
-    b = 32345
-    for a in b:
-        print(a)
     number: str = str(number)
-    str_list = list()
+    digit_list = list()
     for digit in number:
-
+        digit_list.append(int(digit))
+    return digit_list
 
 def get_width(number):
-    if (True):
-        pass
+    if isinstance(number, int) or isinstance(number, str):
+        number = get_digits(number)
+    width = 0
+    for digit in number:
+        if (digit > 1 or digit == 0):
+            width += 5
+        else:
+            width += 3
+        width += 1 # Space between digits
+    return width - 1 # No space after last digit
 
-get_digits(3)
+def get_width_for_multiple_comma_separated_numbers(numbers):
+    width = 0
+    for number in numbers:
+        width += get_width(number)
+        width += 4 # comma + free space around it
+    return width - 4 # no comma after last digit
+
+#numbers = [45, 30, 31]
+#get_width_for_multiple_comma_separated_numbers(numbers)
